f05dc28432c20b49debb59886a0e519c3c85a039
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-04 20:48:38 -0300

    feat: create mock data

---
+++ src/data/capsules.js
@@ -0,0 +1,98 @@
+export default [
+  {
+    id: 1,
+    title: "Conheça mais sobre o Hubblefy",
+    isFixed: true,
+    isRead: true,
+    status: "unsigned",
+    createdAt: "1 hora atrás",
+    notificationCount: 12,
+    category: 2,
+    attachments: [
+      {
+        type: "text",
+        url: "#",
+      },
+      {
+        type: "googlesheet",
+        url: "#",
+      },
+      {
+        type: "googledoc",
+        url: "#",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Conheça mais sobre o Hubblefy",
+    isFixed: true,
+    isRead: true,
+    status: "unsigned",
+    createdAt: "1 hora atrás",
+    notificationCount: 12,
+    category: 2,
+    attachments: [
+      {
+        type: "text",
+        url: "#",
+      },
+      {
+        type: "googlesheet",
+        url: "#",
+      },
+      {
+        type: "googledoc",
+        url: "#",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Conheça mais sobre o Hubblefy",
+    isFixed: true,
+    isRead: true,
+    status: "signed",
+    createdAt: "1 hora atrás",
+    notificationCount: 12,
+    category: 2,
+    attachments: [
+      {
+        type: "text",
+        url: "#",
+      },
+      {
+        type: "googlesheet",
+        url: "#",
+      },
+      {
+        type: "googledoc",
+        url: "#",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Conheça mais sobre o Hubblefy",
+    isFixed: false,
+    isRead: false,
+    status: "signed",
+    createdAt: "1 hora atrás",
+    notificationCount: 12,
+    category: 1,
+    attachments: [
+      {
+        type: "text",
+        url: "#",
+      },
+      {
+        type: "googlesheet",
+        url: "#",
+      },
+      {
+        type: "googledoc",
+        url: "#",
+      },
+    ],
+  },
+];
---
+++ src/data/categories.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    id: 1,
+    title: "Departamento Tech",
+  },
+  {
+    id: 2,
+    title: "Grupo Produto",
+  },
+];
