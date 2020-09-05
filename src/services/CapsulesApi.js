a60d8d84ea2a1b199a96e50065e9e239c1bca80f
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-04 21:23:15 -0300

    feat: create capsules mock api

---
+++ src/services/CapsulesApi.js
@@ -0,0 +1,62 @@
+import capsules from "data/capsules";
+
+const LATENCY = 3000;
+
+const newCapsule = {
+  title: "Conheça mais sobre o Hubblefy",
+  isFixed: false,
+  isRead: false,
+  status: "signed",
+  createdAt: "1 hora atrás",
+  notificationCount: 12,
+  category: 1,
+  attachments: [
+    {
+      type: "text",
+      url: "#",
+    },
+    {
+      type: "googlesheet",
+      url: "#",
+    },
+    {
+      type: "googledoc",
+      url: "#",
+    },
+  ],
+};
+
+export default class CapsulesApi {
+  static getAll() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const storedCapsules = localStorage.getItem("capsules");
+
+        if (storedCapsules) {
+          resolve(JSON.parse(storedCapsules));
+        } else {
+          localStorage.setItem("capsules", JSON.stringify(capsules));
+          resolve(capsules);
+        }
+      }, LATENCY);
+    });
+  }
+
+  static addCapsule() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const storedCapsules = localStorage.getItem("capsules");
+
+        localStorage.setItem(
+          "capsules",
+          JSON.stringify([
+            ...JSON.parse(storedCapsules),
+            { id: storedCapsules.length + 1, ...newCapsule },
+          ])
+        );
+
+        resolve({ status: 201, message: "ok" });
+      }, LATENCY);
+    });
+  }
+}
