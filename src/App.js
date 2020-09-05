77b798ec90a4d86835c94fe3e162db70b040ec34
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-05 01:43:27 -0300

    refactor: add capsules provider

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React from "react";
 import { Helmet } from "react-helmet";
 
 import GlobalStyles from "styles/GlobalStyles";
+import { CapsulesProvider } from "contexts/capsules";
+import Board from "pages/Board";
 
 function App() {
   return (
-    <>
+    <CapsulesProvider>
       <Helmet>
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
       </Helmet>
       <GlobalStyles />
-      <h1>Hubblefy</h1>
-    </>
+      <Board />
+    </CapsulesProvider>
   );
 }
 
