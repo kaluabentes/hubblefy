584a5738b5fe555caa1b27795c3dc98cad4f26f2
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-05 01:49:05 -0300

    feat: create color variables

---
+++ src/styles/colors.js
@@ -0,0 +1,10 @@
+export const COLOR_BLACK = "#191919";
+export const COLOR_ORANGE = "#ff9800";
+export const COLOR_LIGHT_ORANGE = "#86D34a";
+export const COLOR_DOVE_GRAY = "#646464";
+export const COLOR_BRIDAL_HEALTH = "#FFFBF5";
+export const COLOR_BOULDER = "#7C7C7C";
+export const COLOR_RED = "#EB5757";
+export const COLOR_CONCRETE = "#F2F2F2";
+export const COLOR_GRAY = "#828282";
+export const COLOR_MERCURY = "#E5E5E5";
